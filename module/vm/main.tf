8aa6c586cb0f53911b7a23d9e9b1def2349b389b
kahalkar <nikleshkahalkar123@gmail.com>
2026-08-10 13:33:36 +0530

    latest change

--- module/vm/main.tf
+++ module/vm/main.tf
@@ -1,14 +1,23 @@
 resource "azurerm_linux_virtual_machine" "vm" {
-  for_each              = var.vm
-  name                  = each.value.name
-  resource_group_name   = each.value.resource_group
-  location              = each.value.location
-  size                  = each.value.size
-  admin_username        = each.value.admin_username
-  admin_password        = each.value.admin_password
-  network_interface_ids = [data.azurerm_network_interface.nic[each.key].id]
+  for_each = var.vm
+
+  name                = each.value.name
+  resource_group_name = each.value.resource_group
+  location            = each.value.location
+  size                = each.value.size
+
+  admin_username = each.value.admin_username
+
+  network_interface_ids = [
+    data.azurerm_network_interface.nic[each.key].id
+  ]
 
   disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = each.value.admin_username
+    public_key = each.value.ssh_public_key
+  }
 
   os_disk {
     caching              = each.value.caching
