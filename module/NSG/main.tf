9047885760bf0597dca65a7623924c9adfb3c180
kahalkar <nikleshkahalkar123@gmail.com>
2026-08-10 15:48:45 +0530

    2nd latest change

--- module/NSG/main.tf
+++ module/NSG/main.tf
@@ -6,18 +6,18 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = each.value.resource_group
 
   security_rule {
-    name                       = "allow-ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
+    name                   = "allow-ssh"
+    priority               = 100
+    direction              = "Inbound"
+    access                 = "Allow"
+    protocol               = "Tcp"
+    source_port_range      = "*"
+    destination_port_range = "22"
 
     # IMPORTANT:
     # "*" करने पर CKV_AZURE_10 fail होगा.
     # यहाँ अपने trusted/admin network का CIDR डालो.
-    source_address_prefix     = "10.0.0.0/24"
+    source_address_prefix = "10.0.0.0/24"
 
     destination_address_prefix = "*"
   }
