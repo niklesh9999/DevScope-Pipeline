cf74040f3b756e8f6224f8863a0136a5e42a61ff
kahalkar <nikleshkahalkar123@gmail.com>
2026-08-10 13:29:56 +0530

    latest change

--- module/NSG/main.tf
+++ module/NSG/main.tf
@@ -5,15 +5,15 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = each.value.resource_group
 
   security_rule {
-  name                       = "allow-ssh"
-  priority                   = 100
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "Tcp"
-  source_port_range          = "*"
-  destination_port_range     = "22"
-  source_address_prefix      = "YOUR_PUBLIC_IP/32"
-  destination_address_prefix = "*"
-}
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "YOUR_PUBLIC_IP/32"
+    destination_address_prefix = "*"
+  }
 }
 
