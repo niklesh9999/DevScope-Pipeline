87b25d49e5becbbc8ff7dddaeb727200c235ebc8
kahalkar <nikleshkahalkar123@gmail.com>
2026-08-10 15:14:42 +0530

    2nd latest change

--- module/nsga/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_subnet_network_security_group_association" "nsga" {
-  for_each = var.nsga
-
-  subnet_id = var.subnet_id[each.value.subnet_key]
-
-  network_security_group_id = var.network_security_group_id[each.value.nsg_key]
-}
